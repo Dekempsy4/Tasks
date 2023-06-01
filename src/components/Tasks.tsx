36d6aa5a1496056ddcfb90b49e8456cbeb083b9e
Dekempsy4 <felixtoft09@gmail.com>
2023-06-01 17:34:46 +0100

    adding invite button and associated styles

--- src/components/Tasks.tsx
+++ src/components/Tasks.tsx
@@ -11,7 +11,7 @@ import Col from 'react-bootstrap/Col';
 import "../main.css"
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import { TaskList } from "./TaskList"
+import { TaskList } from "./TaskList";
 
 export type Task = {
     type: 'task';
@@ -43,15 +43,16 @@ export const Tasks = <T extends Task = Task>(props: TaskListProps<T>) => {
         <Container fluid id='mainDiv'>
             <Col xs={10} id='mainContent'>
                 <Row id="newEventSection">
-                    <input id='taskListName' value={listTitle} onChange={onTitleChanged}/>
+                    <Col xs={10} id='nameCol'><input id='taskListName' value={listTitle} onChange={onTitleChanged}/></Col>
+                    <Col xs={2} id='invCol'><button id='createInvite'>invite peer</button></Col>
                 </Row>
                 <Row id='taskListRow'><TaskList tasks={tasks} onTaskDeleted={onTaskDeleted} onTaskCompleteChanged={onTaskCompleteChanged}/></Row>
-                    <form onSubmit={onTaskCreate}>
-                        <Row  id='newButtonRow'>
-                            <Col xs={2}><button id='newItemButton'><img src='https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/Plus_symbol.svg/1000px-Plus_symbol.svg.png?20081122110508'></img></button></Col>
-                            <Col xs={10}><input id='newTaskInput' placeholder='New Task'/></Col>
-                        </Row>
-                    </form>       
+                <form onSubmit={onTaskCreate}>
+                    <Row  id='newButtonRow'>
+                        <Col xs={2}><button id='newItemButton'><img src='https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/Plus_symbol.svg/1000px-Plus_symbol.svg.png?20081122110508'></img></button></Col>
+                        <Col xs={10}><input id='newTaskInput' placeholder='New Task'/></Col>
+                    </Row>
+                </form> 
             </Col>
         </Container>
     );
